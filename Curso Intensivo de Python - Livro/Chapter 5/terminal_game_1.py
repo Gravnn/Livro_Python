1a4419a9bd7e50408bee4d9fc5a6f42d71d84696
Gravnn <64759609+Gravnn@users.noreply.github.com>
2022-09-26 21:58:08 -0300

    Add files via upload

--- Curso Intensivo de Python - Livro/Chapter 5/terminal_game_1.py
+++ Curso Intensivo de Python - Livro/Chapter 5/terminal_game_1.py
@@ -5,22 +5,12 @@ classes = ['archer', 'warrior', 'ladin', 'mage', 'paladin']
 print("Type your name")
 name = str(input())
 
-print("Choose your class")
-print("1_ " + classes[0])
-print("2_ " + classes[1])
-print("3_ " + classes[2])
-print("4_ " + classes[3])
-print("5_ " + classes[4])
 
-playerClass = int(input())
-welcome = " is in town. Welcome "
-if playerClass == 1:
-    print("The new " + str(classes[playerClass - 1].lower()) + welcome + name.title().strip() + "!")
-if playerClass == 2:
-    print("The new " + str(classes[playerClass - 1].lower()) + welcome + name.title().strip() + "!")
-if playerClass == 3:
-    print("The new " + str(classes[playerClass - 1].lower()) + welcome + name.title().strip() + "!")
-if playerClass == 4:
-    print("The new " + str(classes[playerClass - 1].lower()) + welcome + name.title().strip() + "!")
-if playerClass == 5:
-    print("The new " + str(classes[playerClass - 1].lower()) + welcome + name.title().strip() + "!")
+print("Choose your class")
+
+for n in range(len(classes)):
+    print(str(n + 1) + "_ " + classes[n].title())
+
+player_class = int(input()) -1
+print("Welcome " + name.title().strip() + ". The " + classes[player_class] + "!")
+
